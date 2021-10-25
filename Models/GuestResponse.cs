ff4c307eb8fc2e3fed3b9d48318ac1a385eefcb5
Airit34 <93119152+Airit34@users.noreply.github.com>
2021-10-25 14:44:48 +0600

    Add files via upload

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+namespace ProgrammerBlog.Models {
+
+    public class GuestResponse {
+
+        [Required(ErrorMessage = "Please enter your name:")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address:")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Please enter a valid email address:")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number:")]
+        public string Phone { get; set; }
+    }
+}
